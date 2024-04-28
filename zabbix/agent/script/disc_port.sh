c5108d27a0f1c9b387bb553b4570df02a60618dd
YangJeff <yangjh@kunpan.com>
2024-04-28 14:17:29 +0800

    update

--- zabbix/agent/script/disc_port.sh
+++ zabbix/agent/script/disc_port.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-PortName=(6379 7610 8611 8616 7613 8615 7617 7618 7619 7620 7621 7622 7623 8618)
-ProName=(redis-6379 bs-7610 cs-8611 dndss-8616 gms-7613 groups-8615 gs-7617 gs1-7618 gs2-7619 gs3-7620 gs4-7621 gs5-7622 gs6-7623 ks-8618)
+PortName=(6379)
+ProName=(redis-6379)
 Include=$(echo ${PortName[*]} | sed -e "s/ /|/g")
 PortNum=(`/usr/sbin/ss -tnlp | egrep -i "$1" | awk {'print $4'} | awk -F':' '{if ($NF~/^[0-9]*$/) print $NF}' | grep -o -E "$Include" |sort|uniq  2>/dev/null`)
 Length=${#PortNum[@]}
